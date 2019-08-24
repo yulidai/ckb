5123837a6ebf92c7d28ba184c2788d34b19a32d4
quake wang <quake.wang@gmail.com>
2019-08-24 16:35:46 +0800

    refactor: change `Header#dao` to Byte32

--- benches/benches/benchmarks/util.rs
+++ benches/benches/benchmarks/util.rs
@@ -61,7 +61,7 @@ pub fn new_always_success_chain(txs_size: usize, chains_num: usize) -> Chains {
         .collect();
 
     let genesis_block = BlockBuilder::default()
-        .dao(dao.pack())
+        .dao(dao)
         .difficulty(U256::from(1000u64).pack())
         .transaction(tx)
         .transactions(transactions)
@@ -178,7 +178,7 @@ pub fn gen_always_success_block(
         .timestamp(timestamp.pack())
         .difficulty(difficulty.pack())
         .nonce(random::<u64>().pack())
-        .dao(dao.pack())
+        .dao(dao)
         .build();
 
     blocks.push(block.clone());
@@ -266,7 +266,7 @@ pub fn new_secp_chain(txs_size: usize, chains_num: usize) -> Chains {
 
     let genesis_block = BlockBuilder::default()
         .difficulty(U256::from(1000u64).pack())
-        .dao(dao.pack())
+        .dao(dao)
         .transaction(tx)
         .transactions(transactions)
         .build();
@@ -375,7 +375,7 @@ pub fn gen_secp_block(
         .timestamp(timestamp.pack())
         .difficulty(difficulty.pack())
         .nonce(random::<u64>().pack())
-        .dao(dao.pack())
+        .dao(dao)
         .build();
 
     blocks.push(block.clone());
